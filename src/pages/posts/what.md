d9c5e4d09d95bea71947351d0acb4811095668d4
vuminhtrinh2804 <vuminhtrinh2804@gmail.com>
2022-11-09 07:35:28 +0700

    init

---
+++ src/pages/posts/what.md
@@ -0,0 +1,14 @@
+---
+layout: "@/layouts/BlogPost"
+title: My Third Post
+publishDate: 12 June 2022
+description: It's another post by me.
+tags: ["example", "blog"]
+value: 128
+---
+
+## Sick ducking
+- hello world
+## No whit
+- WHas asdjn dcjn
+
---
+++ src/site-meta.config.ts
@@ -0,0 +1,11 @@
+const siteMeta = {
+  title: "tminhvu's blog",
+  description: "where I litter the internet with random articles",
+  lang: "vn_VN",
+  ogLocale: "vn_VN",
+  githubUrl: "https://github.com/tminhvu",
+  themeColorLight: "#fafafa",
+  themeColorDark: "#1d1f21",
+}
+
+export default siteMeta;
